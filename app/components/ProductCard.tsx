cb91b1864663d656f89a1f30da09174c122fd182
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-08-16 18:57:53 +0530

    Addressed review comments and made necessary changes

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,28 +1,14 @@
 "use client";
 import React from 'react';
+import { IProductCardProps } from '../types/product';
 
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
-}
-
-interface ProductCardProps {
-    product: Product;
-    currency: string; // e.g., 'USD', 'EUR', 'JPY'
-  }
-
-  const ProductCard = ({ product, currency }: ProductCardProps) => {
+  const ProductCard = ({ product, currency }: IProductCardProps) => {
 
     const formattedPrice = new Intl.NumberFormat('en-IN', {
         style: 'currency',
         currency: currency,
       }).format(product.price);
 
-    const addToCart = () => {
-    console.log(`${product.title} added to cart`);
-  };
 
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden ">
@@ -31,7 +17,7 @@ interface ProductCardProps {
         <h2 className="text-lg font-semibold text-black truncate">{product.title}</h2>
         <p className="text-gray-700">{formattedPrice}</p>
         <button
-          onClick={addToCart}
+        //   onClick={addToCart}
           className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
         >
           Add to Cart
