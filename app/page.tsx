621807e9579f8653660f4e216574b66b55df375a
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-08-16 18:57:53 +0530

    Addressed review comments and made necessary changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
-import ProductCard from './components/ProductCard';
+import ProductCard from "./components/ProductCard";
+import { IProduct } from "./types/product";
 
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
-}
 
 export default async function Home() {
-  const response = await fetch('https://fakestoreapi.com/products?limit=10');
-  const products: Product[] = await response.json();
+  let products: IProduct[] = [];
 
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products?limit=10",
+      {}
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch products");
+    }
+    products = await response.json();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
   return (
     <div className="container mx-auto px-4 py-8 ">
       <h1 className="text-3xl font-bold mb-6">Product Listing</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} currency='INR' />
+          <ProductCard key={product.id} product={product} currency="INR" />
         ))}
       </div>
     </div>
