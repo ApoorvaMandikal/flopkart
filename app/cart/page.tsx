665adc1e651dd5fd152b0464021750f6d7d91880
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-08-17 13:51:33 +0530

    feat: Added a cart is empty message and handled add to cart button change

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FaceFrownIcon } from '@heroicons/react/24/outline'
 
 const page = () => {
   return (
     <div>
-      
+       <div className="container mx-auto px-4 py-8">
+       <h1 className="text-3xl font-bold mb-6">Shopping Cart</h1>
+       <div className='flex flex-col items-center'>
+       <p >Your cart is empty! </p>
+       <p><FaceFrownIcon className='size-10'></FaceFrownIcon></p>
+       </div>
+       </div>
     </div>
   )
 }
--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,30 +1,52 @@
 "use client";
-import React from 'react';
-import { IProductCardProps } from '../types/product';
+import React, { useState } from "react";
+import { IProductCardProps } from "../types/product";
 
-  const ProductCard = ({ product, currency }: IProductCardProps) => {
+const ProductCard = ({ product, currency }: IProductCardProps) => {
+  const [added, setAdded] = useState(false);
 
-    const formattedPrice = new Intl.NumberFormat('en-IN', {
-        style: 'currency',
-        currency: currency,
-      }).format(product.price);
+  const handleAddtocart = () => {
+    setAdded(true);
+  };
 
+  const formattedPrice = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: currency,
+  }).format(product.price);
 
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden ">
-      <img src={product.image} alt={product.title} className="w-full h-48 object-contain" />
+      <img
+        src={product.image}
+        alt={product.title}
+        className="w-full h-48 object-contain"
+      />
       <div className="p-4">
-        <h2 className="text-lg font-semibold text-black truncate">{product.title}</h2>
+        <h2 className="text-lg font-semibold text-black truncate">
+          {product.title}
+        </h2>
         <p className="text-gray-700">{formattedPrice}</p>
-        <button
-        //   onClick={addToCart}
-          className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-          Add to Cart
-        </button>
+        {added ? (
+          <div className="flex items-center mt-4 w-full">
+            <button className="bg-blue-500 text-white py-2 px-4 rounded-l hover:bg-blue-600">
+              -
+            </button>
+            <span className="px-4 py-2 w-full text-center">1</span>
+            <button className="bg-blue-500 text-white py-2 px-4 rounded-r hover:bg-blue-600">
+              +
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={handleAddtocart}
+            className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 w-full"
+          >
+            Add to Cart
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default ProductCard;
