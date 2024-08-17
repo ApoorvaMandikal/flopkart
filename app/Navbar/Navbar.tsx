8979e8981b55aedde29fce5805f175e07ad2f91a
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-08-17 13:41:09 +0530

    feat: added conditional rendering of navbar and categories bar for cart page

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 "use client"; // This makes the component a Client Component
 
 import React from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav
       className="text-white flex items-center h-14"
@@ -16,7 +19,6 @@ const Navbar = () => {
         <div className="text-xl italic font-bold pl-4 lg:pl-16 mr-4">
           <Link href="/">Flopkart</Link>
         </div>
-
         <div className="flex-grow relative max-w-md">
           <input
             type="text"
@@ -25,28 +27,30 @@ const Navbar = () => {
           />
           <MagnifyingGlassIcon className="absolute text-blue-500 h-6 w-6 right-4 top-2" />
         </div>
-
         <div className="hidden sm:flex items-center mx-2 lg:mx-4">
           <button className="bg-white text-blue-500 font-bold px-4 py-2 hover:bg-blue-600 hover:text-white w-24 sm:w-28 lg:w-32 h-8 flex items-center justify-center">
             Login
           </button>
         </div>
 
+        {pathname !== "/cart" && (
+          <>
         <div className="hidden lg:flex text-base font-semibold space-x-4 mx-2 lg:mx-4">
           <Link href="/">Become a Seller</Link>
         </div>
-
         <div className="hidden lg:flex items-center text-base font-semibold mx-2 lg:mx-4">
           <Link href="/">More</Link>
           <ChevronDownIcon className="text-white h-4 w-4 ml-1" />
         </div>
-
-        <div className="sm:flex items-center mx-2 lg:mx-4 text-base font-semibold">
-          <Link href="/cart" className="flex justify-between text-center px-3 py-2 ">
-            <ShoppingCartIcon className="text-white size-6 py-1"></ShoppingCartIcon>
-            <p>Cart</p>
-          </Link>
-        </div>
+        
+          <div className="sm:flex items-center mx-2 lg:mx-4 text-base font-semibold">
+            <Link href="/cart" className="flex justify-between text-center px-3 py-2">
+              <ShoppingCartIcon className="text-white size-6 py-1" />
+              <p>Cart</p>
+            </Link>
+          </div>
+        </>
+        )}
       </div>
     </nav>
   );
--- app/components/CategoriesBar.tsx
+++ app/components/CategoriesBar.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import React from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { usePathname } from "next/navigation";
 
 const CategoriesBar = () => {
+  const pathname = usePathname();
   const categories = [
     "Electronics",
     "TVs & Appliances",
@@ -13,24 +17,29 @@ const CategoriesBar = () => {
   ];
 
   return (
-    <div className="hidden lg:block bg-white text-black drop-shadow">
-      <div className="flex justify-between items-center px-6 py-2 max-w-6xl mx-auto">
-        {categories.map((category, index) => (
-          <div
-            key={index}
-            className="flex items-center text-xs lg:text-sm font-semibold cursor-pointer space-x-2"
-          >
-            <span>{category}</span>
-            <ChevronDownIcon className="text-gray-500 size-3" />
+    <>
+      {pathname !== "/cart" && (
+        <div className="hidden lg:block bg-white text-black drop-shadow">
+          <div className="flex justify-between items-center px-6 py-2 max-w-6xl mx-auto">
+            {categories.map((category, index) => (
+              <div
+                key={index}
+                className="flex items-center text-xs lg:text-sm font-semibold cursor-pointer space-x-2"
+              >
+                <span>{category}</span>
+                <ChevronDownIcon className="text-gray-500 size-3" />
+              </div>
+            ))}
+            <div className="flex items-center space-x-2 text-sm lg:text-sm font-semibold cursor-pointer">
+              <span>Flights</span>
+            </div>
+            <div className="flex items-center space-x-2 text-sm lg:text-sm font-semibold cursor-pointer">
+              <span>Offer Zone</span>
+            </div>
           </div>
-        ))}
-        <div className="flex items-center space-x-2 text-sm lg:text-sm font-semibold cursor-pointer">
-          <span>Flights</span></div>
-          <div className="flex items-center space-x-2 text-sm lg:text-sm font-semibold cursor-pointer">
-          <span>Offer Zone</span>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
